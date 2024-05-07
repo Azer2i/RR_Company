b39039651f818a5d8b0664552a6d80ed33a03593
Azer Ibrahimli <aibrahimov191@gmail.com>
2024-05-07 15:26:26 +0400

    version1

---
+++ src/test/java/com/example/rr_company/RrCompanyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rr_company;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RrCompanyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
